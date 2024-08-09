b0073cd90a686a14700bf9c7de4b249b95476f54
muaztalhaBulut <muaztalha.bulut@gappze.com>
2024-08-09 14:41:27 +0300

    update animation access level

--- Sources/SmartPaywall/Paywall/Animation/AnimationManager.swift
+++ Sources/SmartPaywall/Paywall/Animation/AnimationManager.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AnimationManager {
+public class AnimationManager {
     static let shared = AnimationManager()
     
     private init() {}
--- Sources/SmartPaywall/Paywall/Animation/AnimationType.swift
+++ Sources/SmartPaywall/Paywall/Animation/AnimationType.swift
@@ -5,7 +5,7 @@
 //  Created by Talha on 25.08.2023.
 //
 
-enum AnimationType: String {
+public enum AnimationType: String {
     case heartbeat
     case vibration
 }
