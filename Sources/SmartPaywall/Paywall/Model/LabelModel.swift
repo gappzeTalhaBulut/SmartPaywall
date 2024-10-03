20f180fec7d9710bcc48f06668bf0fccbdeb0aff
muaztalhaBulut <muaztalha.bulut@gappze.com>
2024-10-03 11:34:51 +0300

    Update LabelModel.swift

--- Sources/SmartPaywall/Paywall/Model/LabelModel.swift
+++ Sources/SmartPaywall/Paywall/Model/LabelModel.swift
@@ -33,8 +33,8 @@ struct LabelModel: Decodable {
         self.textAlignment = try container.decodeIfPresent(TextAlignmentType.self, forKey: .textAlignment) ?? .center
         self.attributes = try container.decode([TextAttributeModel].self, forKey: .attributes)
         self.isVisible = try container.decodeIfPresent(Bool.self, forKey: .isVisible) ?? true
-        self.multiplier = try container.decode(Double.self, forKey: .multiplier)
-        self.multiplier2 = try container.decode(Double.self, forKey: .multiplier2)
+        self.multiplier = try container.decodeIfPresent(Double.self, forKey: .multiplier) ?? 1.0
+        self.multiplier2 = try container.decode(Double.self, forKey: .multiplier2) ?? 1.0
     }
 }
 
