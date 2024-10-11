a83038b18fa248f7897a8bffc3ed9e1647354a4d
muaztalhaBulut <muaztalha.bulut@gappze.com>
2024-10-11 11:00:35 +0300

    Update HorizontalSelectionView.swift

--- Sources/SmartPaywall/Paywall/UIComponent/HorizontalSelectionView.swift
+++ Sources/SmartPaywall/Paywall/UIComponent/HorizontalSelectionView.swift
@@ -78,9 +78,9 @@ final class HorizontalSelectionView: UICollectionView, UICollectionViewDelegate,
         model.productList = productList
         collectionView.performBatchUpdates {
             // Bu işlemler collectionView'in boyutları güncellenirken yapılır
+            self.didSelect?(selectedProductId)
         } completion: { _ in
             // Güncelleme bittikten sonra seçili ürünü bildir
-            self.didSelect?(selectedProductId)
         }
     }
 }
